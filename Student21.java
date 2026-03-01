a26599c7e684b346b3b6161fda4b0f3d58cc4f06
“selpiiee” <selvynurkhoffifa@gmail.com>
2026-03-01 10:20:00 +0700

    Practicum 3.3

--- Student21.java
+++ Student21.java
@@ -1,6 +1,16 @@
+
 public class Student21 {
+
     public String name;
     public String nim;
     public String Class;
     public float gpa;
+
+    public void printInfo() {
+        System.out.println("NIM\t: " + nim);
+        System.out.println("Name\t: " + name);
+        System.out.println("Class\t: " + Class);
+        System.out.println("GPA\t: " + gpa);
+        System.out.println("-----------------------------");
+    }
 }
