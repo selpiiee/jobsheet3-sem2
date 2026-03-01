ff8727c095ccb1df4c2ef404fb03d7cc48ebc7db
“selpiiee” <selvynurkhoffifa@gmail.com>
2026-03-01 10:20:14 +0700

    Practicum 3.3 - Main

--- StudentDemo21.java
+++ StudentDemo21.java
@@ -1,39 +1,36 @@
+import java.util.Scanner;
+
 public class StudentDemo21 {
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Student21[] arrayOfStudents = new Student21[3];
-        arrayOfStudents[0] = new Student21();
-        arrayOfStudents[0].nim = "24410700033";
-        arrayOfStudents[0].name = "AGNES TITANIA KINANTI";
-        arrayOfStudents[0].Class = "SIB-1E";
-        arrayOfStudents[0].gpa = (float) 3.75;
+        String dummy;
 
-        arrayOfStudents[1] = new Student21();
-        arrayOfStudents[1].nim = "2341720172";
-        arrayOfStudents[1].name = "ACHMAD MAULANA HAMZAH";
-        arrayOfStudents[1].Class = "TI-2A";
-        arrayOfStudents[1].gpa = (float) 3.36;
+        for (int i = 0; i < 3; i++) {
+            arrayOfStudents[i] = new Student21();
 
-        arrayOfStudents[2] = new Student21();
-        arrayOfStudents[2].nim = "244107023006";
-        arrayOfStudents[2].name = "DIRHAMAWAN PUTRANTO";
-        arrayOfStudents[2].Class = "TI-2E";
-        arrayOfStudents[2].gpa = (float) 3.80;
+            System.out.println("Enter student data  " + (i + 1));
+            System.out.print("NIM\t: ");
+            arrayOfStudents[i].nim = sc.nextLine();
+            System.out.print("NAME\t: ");
+            arrayOfStudents[i].name = sc.nextLine();
+            System.out.print("CLASS\t: ");
+            arrayOfStudents[i].Class = sc.nextLine();
+            System.out.print("GPA\t: ");
+            dummy = sc.nextLine();
+            arrayOfStudents[i].gpa = Float.parseFloat(dummy);
+            System.out.println("-----------------------------");
+        }
 
-
-        System.out.println("NIM\t: " + arrayOfStudents[0].nim);
-        System.out.println("NAME\t: " + arrayOfStudents[0].name);
-        System.out.println("CLASS\t: " + arrayOfStudents[0].Class);
-        System.out.println("GPA\t: " + arrayOfStudents[0].gpa);
-        System.out.println("-------------------------------------");
-        System.out.println("NIM\t: " + arrayOfStudents[1].nim);
-        System.out.println("NAME\t: " + arrayOfStudents[1].name);
-        System.out.println("CLASS\t: " + arrayOfStudents[1].Class);
-        System.out.println("GPA\t: " + arrayOfStudents[1].gpa);
-        System.out.println("-------------------------------------");
-        System.out.println("NIM\t: " + arrayOfStudents[2].nim);
-        System.out.println("NAME\t: " + arrayOfStudents[2].name);
-        System.out.println("CLASS\t: " + arrayOfStudents[2].Class);
-        System.out.println("GPA\t: " + arrayOfStudents[2].gpa);
-        System.out.println("-------------------------------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Student data  " + (i + 1));
+            System.out.println("NIM\t: " + arrayOfStudents[i].nim);
+            System.out.println("NAME\t: " + arrayOfStudents[i].name);
+            System.out.println("CLASS\t: " + arrayOfStudents[i].Class);
+            System.out.println("GPA\t: " + arrayOfStudents[i].gpa);
+            System.out.println("-----------------------------");
+        }
     }
+
 }
